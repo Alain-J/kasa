07ba8a0118de2d6b9b7023b777a7489eec647d6b
Alain-J <alaindu1383@icloud.com>
2022-11-14 09:13:43 +0100

    suites

---
+++ src/Routes/RoutesPath.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from '../composants/Layout';
+import Home from '../pages/Home';
+import Erreur from '../pages/Erreur'
+import Logement from '../pages/Logement';
+import APropos from '../pages/APropos';
+
+
+function RoutesPath() {
+  return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/logement/:id" element={<Logement/>}/>
+                    <Route path="/a-propos" element={<APropos/>}/>
+                    <Route path="*" element={<Erreur/>}/>
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+  )
+}
+
+export default RoutesPath
